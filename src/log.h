c471a38a64ed4236317baf56949cdc7c2e8d2e56
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2020-10-04 03:15:56 +0900

    Horrible of having logs in Android for my project but... It's temporary!

--- src/log.h
+++ src/log.h
@@ -31,12 +31,23 @@ typedef void (*log_LockFn)(bool lock, void *udata);
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
 #ifndef NDEBUG
+#ifndef CENGINE_ANDROID
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define log_info(...)  log_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
 #define log_warn(...)  log_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
 #define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+#else
+#include <android/log.h>
+#define log_trace(...) ((void)__android_log_print(ANDROID_LOG_INFO, "cengine", __VA_ARGS__))
+#define log_debug(...) ((void)__android_log_print(ANDROID_LOG_INFO, "cengine", __VA_ARGS__))
+#define log_info(...)  ((void)__android_log_print(ANDROID_LOG_INFO, "cengine", __VA_ARGS__))
+#define log_warn(...)  ((void)__android_log_print(ANDROID_LOG_WARN, "cengine", __VA_ARGS__))
+#define log_error(...) ((void)__android_log_print(ANDROID_LOG_WARN, "cengine", __VA_ARGS__))
+#define log_fatal(...) ((void)__android_log_print(ANDROID_LOG_WARN, "cengine", __VA_ARGS__))
+#endif // CENGINE_ANDROID
+
 #else
 #define log_trace(...)
 #define log_debug(...)
